64ea418cbf66bf97825c35f486cd04042af5ce88
lucyjv <luci_jv@hotmail.com>
2025-05-04 19:32:38 -0300

    .

--- database-admin.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE administradores(
-    id_incremental INT(11) PRIMARY KEY AUTO_INCREMENT,
-    usuario varchar(100),
-    contrasenia varchar(100)
-);
-
-INSERT INTO administradores(usuario, contrasenia) VALUES('admin', '1234');
